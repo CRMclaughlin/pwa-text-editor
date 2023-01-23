9924fbbd97467d9df7f4d80fab31c989270eac6e
CRMclaughlin <chrisraymusik@gmail.com>
2023-01-22 19:59:01 -0600

    trying to debug icons 404

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -56,9 +56,8 @@ module.exports = () => {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: [
-                ['@babel/preset-env', { targets: "defaults" }]
-              ]
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             }
           }
         }
